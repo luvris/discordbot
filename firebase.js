5b5e2e8c967f5d0808d98367011815b3b1faa18a
luvris <denluvris@gmail.com>
2026-04-13 18:38:44 +0700

    fix: use env for firebase credentials

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccount.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
